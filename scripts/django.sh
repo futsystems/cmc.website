c5808795f1efe0b9ad84f4772afed2d095ae7585
Vincent <qianbo@futsystems.com>
2020-08-19 18:39:01 +0800

    Update django.sh

--- scripts/django.sh
+++ scripts/django.sh
@@ -13,10 +13,10 @@ TIMEOUT=30
 echo "Starting $NAME"
 
 # Activate the virtual environment
-cd $DJANGODIR
-source /opt/cmc.website/venv/bin/activate
-export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
-export PYTHONPATH=$DJANGODIR:$PYTHONPATH
+#cd $DJANGODIR
+#source /opt/cmc.website/venv/bin/activate
+#export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
+#export PYTHONPATH=$DJANGODIR:$PYTHONPATH
 
 # Create the run directory if it doesn't exist
 RUNDIR=$(dirname $SOCKFILE)
@@ -24,13 +24,11 @@ test -d $RUNDIR || mkdir -p $RUNDIR
 
 # Start your Django Unicorn
 # Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
-exec gunicorn ${DJANGO_WSGI_MODULE}:application \
-  --name $NAME \
-  --workers $NUM_WORKERS \
-  --timeout $TIMEOUT \
-  #-k gevent \
-  --user=$USER --group=$GROUP \
-  --log-level=debug \
-  #--bind=unix:$SOCKFILE \
-  --bind=0.0.0.0:80 \
-  --log-file=$LOGFILE
+echo "!!!!!!!!"
+
+python manage.py migrate
+python manage.py collectstatic --noinput
+#gunicorn $WEB_APP.wsgi:application -w $WEB_APP_WORKERS -b 0.0.0.0:$WEB_APP_PORT
+
+
+gunicorn ${DJANGO_WSGI_MODULE}:application --name $NAME --workers $NUM_WORKERS --timeout $TIMEOUT --user=$USER --group=$GROUP --log-level=debug --bind=0.0.0.0:80 --log-file=$LOGFILE
