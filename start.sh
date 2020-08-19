1b9ed1c4971513e78acdcbd98ecd9e94de643be8
Vincent <qianbo@futsystems.com>
2020-08-19 12:10:39 +0800

    .

--- start.sh
+++ start.sh
@@ -11,7 +11,7 @@ WEB_APP=cmc
 
 set -e
 
-echo "start django website:$WEB_APP"
+echo "start django website:$WEB_APP at port:$WEB_APP_PORT"
 
 
 
@@ -19,12 +19,3 @@ python manage.py migrate
 python manage.py collectstatic --noinput
 gunicorn $WEB_APP.wsgi:application -w $WEB_APP_WORKERS -b 0.0.0.0:$WEB_APP_PORT
 
-
-
-
-##!/bin/sh
-
-#python manage.py migrate
-#python manage.py collectstatic --noinput
-#gunicorn ms_platform.wsgi:application -w 1 -k gthread -b 0.0.0.0:8000 --chdir=/app/website/src
-#gunicorn ms_platform.wsgi:application -w 1 -b 0.0.0.0:8000 --chdir=/app/bin/
