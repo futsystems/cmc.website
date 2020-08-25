c2657f69b288a6aaf04e890ab407e1f82ea9ef05
Vincent <qianbo@futsystems.com>
2020-08-25 23:09:19 +0800

    Update salt_helper.py

--- common/salt_helper.py
+++ common/salt_helper.py
@@ -1,28 +1,11 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/python
 
-import salt
 
-#import salt.client
-#import salt.config
-#import salt.wheel
-#import salt.runner
 import logging
-import urllib2
-import json
 
-import os.path
 
 logger = logging.getLogger(__name__)
-
-#if os.path.isfile('/etc/salt/master'):
-#    opts = salt.config.master_config('/etc/salt/master')
-#else:
-#    opts = None
-#wheel = salt.wheel.WheelClient(opts)
-
-#runner = salt.runner.RunnerClient(opts)
-
 
 
 def highstate(server):
