ca0f15b1c3768c7b08008b7cc224d86d1f78e8f3
Vincent <qianbo@futsystems.com>
2020-08-26 11:00:48 +0800

    Update django.sh

--- scripts/django.sh
+++ scripts/django.sh
@@ -28,9 +28,9 @@ echo "!!!!!!!!"
 
 python manage.py migrate
 python manage.py collectstatic --noinput
-python manage.py runserver 0.0.0.0:80
+#python manage.py runserver 0.0.0.0:80
 
-#gunicorn $WEB_APP.wsgi:application -w $WEB_APP_WORKERS -b 0.0.0.0:$WEB_APP_PORT
+#unicorn $WEB_APP.wsgi:application -w $WEB_APP_WORKERS -b 0.0.0.0:$WEB_APP_PORT
 
 
 #gunicorn ${DJANGO_WSGI_MODULE}:application --name $NAME --workers $NUM_WORKERS --timeout $TIMEOUT --user=$USER --group=$GROUP --log-level=debug --bind=0.0.0.0:80 --log-file=$LOGFILE
